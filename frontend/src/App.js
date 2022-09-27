bb759317adce1e07a3ad96b96dabb0eef7ee4d21
OmarAlduais <omar.a.alduais@gmail.com>
2022-09-27 08:27:59 +0300

    frontend register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+//----------------------------
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Navbar/>
+      <Routes>
+      <Route path="/register" element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,6 +1,52 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+//-----------------------------------------
 
 const Register = () => {
-  return <div>Register</div>;
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const role = '6331a7286b100154eb282d18';
+ const [message, setMessage] = useState('');
+
+//------------------------------------------
+const createUser = () => {
+  axios.post('http://localhost:5000/users/', {
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    password,
+    role,
+
+  }).then((res)=>{
+    setMessage(res.data.message)
+  }).catch((err)=>{
+    setMessage(err.response.data.message)
+  })
+  
+}
+//------------------------------------------
+
+  return <div>Register
+    <input onChange={(e)=>{setFirstName(e.target.value)}} type={'text'} placeholder ={'First Name'}></input>
+    
+    <input onChange={(e)=>{setLastName(e.target.value)}} type={'text'} placeholder ={'Last Name'}></input>
+
+    <input onChange={(e)=>{setAge(e.target.value)}} type={'text'} placeholder ={'Age'}></input>
+
+    <input onChange={(e)=>{setCountry(e.target.value)}} type={'text'} placeholder ={'Country'}></input>
+
+    <input onChange={(e)=>{setEmail(e.target.value)}} type={'text'} placeholder ={'Email'}></input>
+
+    <input onChange={(e)=>{setPassword(e.target.value)}} type={'text'} placeholder ={'Password'}></input>
+
+    <button onClick={createUser}>Register</button>
+  </div>;
 };
 export default Register;
